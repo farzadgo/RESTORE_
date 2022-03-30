9da4ffe7b6237a6019a4b7891000d8bbe7bf7a6a
farzadgo <farzadesign@gmail.com>
2022-03-31 01:32:34 +0200

    meta image link updated

--- components/Meta.js
+++ components/Meta.js
@@ -9,7 +9,7 @@ const Meta = () => {
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
       <meta name="theme-color" content="#0000ff"></meta>
-      <meta name="image" content="/og.png" />
+      <meta name="image" content="https://restore-bremen.space/og.png" />
 
       <title>{title}</title>
       <link rel="icon" href="/favicon.ico" />
